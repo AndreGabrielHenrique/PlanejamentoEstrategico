717ff8a9b0806ff0c1cb8326803a0691f5c1d8c7
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-06-09 12:39:56 -0300

    Add readme.md

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿/*1- Elabore um planejamento estratégico dessa vez utilizando
-todos os comandos aprendidos, e com no mínimo 5 condições
-relacionadas ao planejamento.*/
-class Program
+﻿class Program
 {
     //Estruturar o objeto Projeto
     class Projeto
---
+++ readme.md
@@ -0,0 +1,3 @@
+1- Elabore um planejamento estratégico dessa vez utilizando
+todos os comandos aprendidos, e com no mínimo 5 condições
+relacionadas ao planejamento.
